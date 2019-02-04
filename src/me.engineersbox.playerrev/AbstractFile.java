cdacd6e126a1c174031dea0fd14a6ec50641e48f
Jack Kilrain <toastcraft.info@gmail.com>
2019-02-04 16:28:03 +0100

    Added AbstractFile.java

---
+++ src/me.engineersbox.playerrev/AbstractFile.java
@@ -0,0 +1,39 @@
+package me.engineersbox.playerrev;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import me.engineersbox.playerrev.Main;
+
+public class AbstractFile {
+   
+    protected static Main main;
+    static File file;
+    protected static FileConfiguration config;
+   
+    public AbstractFile(Main main, String filename) {
+        AbstractFile.main = main;
+        AbstractFile.file = new File(main.getDataFolder(), filename);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        AbstractFile.config = YamlConfiguration.loadConfiguration(file);
+        config.addDefault("new", "");
+        config.options().copyDefaults(true);
+    }
+    
+    public static void saveConfig() {
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+   
+}
