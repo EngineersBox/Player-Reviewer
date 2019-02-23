ee09ade9077ca773531998f58c9806c92b7a08b7
Jack Kilrain <toastcraft.info@gmail.com>
2019-02-23 14:15:18 +0100

    Added new exception FieldValueException

---
+++ src/MethodLib/FieldValueException.java
@@ -0,0 +1,15 @@
+package MethodLib;
+
+public class FieldValueException extends Exception {
+
+	private static final long serialVersionUID = 302625609726858424L;
+	
+	public FieldValueException(String field) {
+		super(field);
+	}
+
+	public String toString() {
+		return this.toString();
+	}
+
+}
