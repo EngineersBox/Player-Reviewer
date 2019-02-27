df9d4215535fca2474cf05a3148297813d069a6e
Jack Kilrain <toastcraft.info@gmail.com>
2019-02-27 13:42:08 +0100

    Added InvalidGroupException

---
+++ src/me.engineersbox.playerrev.exceptions/InvalidGroupException.java
@@ -0,0 +1,15 @@
+package me.engineersbox.playerrev.exceptions;
+
+public class InvalidGroupException extends Exception {
+
+	private static final long serialVersionUID = 4765422052440305857L;
+
+	public InvalidGroupException(String field) {
+		super(field);
+	}
+
+	public String toString() {
+		return this.toString();
+	}
+	
+}
