bec1d0f17091b535ba081e315c3d5b402caf3b7f
Jack Kilrain <toastcraft.info@gmail.com>
2019-02-27 13:41:22 +0100

    moved FieldValueException to #exceptions package

---
+++ src/me.engineersbox.playerrev.exceptions/FieldValueException.java
@@ -0,0 +1,15 @@
+package me.engineersbox.playerrev.exceptions;
+
+public class FieldValueException extends Exception {
+
+	private static final long serialVersionUID = 302625609726858424L;
+	
+	public FieldValueException(String field) {
+		super(field);
+	}
+
+	public String toString() {
+		return this.toString();
+	}
+
+}
