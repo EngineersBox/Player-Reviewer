b982e6880a3162b8ed25962af07ae612c2c8cba1
Jack Kilrain <toastcraft.info@gmail.com>
2019-03-01 11:14:42 +0100

    Added HoverMessage and HoverMessageAllPlayers with general argument methods

---
+++ src/me.engineersbox.playerrev.methodlib/HoverText.java
@@ -0,0 +1,57 @@
+package me.engineersbox.playerrev.methodlib;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.chat.ComponentSerializer;
+
+public class HoverText {
+	
+	public static void HoverMessage(Player p, String displayText, List<String> hoverContent) {
+		
+		ArrayList<Object> components = new ArrayList<>();
+		TextComponent comp = new TextComponent(Lib.format(displayText));
+		TextComponent hoverMessage = new TextComponent("");
+		TextComponent newLine = new TextComponent(ComponentSerializer.parse("{text: \"\n\"}"));
+		
+		for (String content : hoverContent) {
+			hoverMessage.addExtra(newLine);
+			hoverMessage.addExtra(new TextComponent(new ComponentBuilder(Lib.format(content)).create()));
+		}
+		
+		components.add(hoverMessage);
+		BaseComponent[] hoverToSend = (BaseComponent[])components.toArray(new BaseComponent[components.size()]);
+		comp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverToSend));
+		p.spigot().sendMessage(comp);
+		
+	}
+	
+	public static void HoverMessageAllPlayers(String displayText, List<String> hoverContent) {
+		
+		ArrayList<Object> components = new ArrayList<>();
+		TextComponent comp = new TextComponent(Lib.format(displayText));
+		TextComponent hoverMessage = new TextComponent("");
+		TextComponent newLine = new TextComponent(ComponentSerializer.parse("{text: \"\n\"}"));
+		
+		for (String content : hoverContent) {
+			hoverMessage.addExtra(newLine);
+			hoverMessage.addExtra(new TextComponent(new ComponentBuilder(Lib.format(content)).create()));
+		}
+		
+		components.add(hoverMessage);
+		BaseComponent[] hoverToSend = (BaseComponent[])components.toArray(new BaseComponent[components.size()]);
+		comp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverToSend));
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			p.spigot().sendMessage(comp);
+		}
+		
+	}
+	
+}
