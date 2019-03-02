8da5bf55fd1c7e1ec3cec0590515b2466eb47068
Jack Kilrain <toastcraft.info@gmail.com>
2019-03-02 17:17:44 +0100

    Added #InitRankConfig()

--- src/me.engineersbox.playerrev.mysql/SQLConfig.java
+++ src/me.engineersbox.playerrev.mysql/SQLConfig.java
@@ -1,5 +1,9 @@
 package me.engineersbox.playerrev.mysql;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 
 import me.engineersbox.playerrev.AbstractFile;
@@ -36,11 +40,33 @@ public class SQLConfig extends AbstractFile {
 	public static boolean SQLEnabled() {
 		if (config.getBoolean("User-Details.UseSQL") == true) {
 			Bukkit.getLogger().info("[Player Reviewer] SQL Interfacting Enabled");
-			return config.getBoolean("User-Details.UseSQL");
+			return true;
 		} else {
 			Bukkit.getLogger().info("[Player Reviewer] SQL Interfacting Disabled, Using Local File: " + config.getName());
-			return config.getBoolean("User-Details.UseSQL");
+			return false;
 		}
+	}
+	
+	public static void InitRankConfig() {
+		Main.useConfigRanks = config.getBoolean("Application-Settings.Use-Config-Ranks");
+    	if (Main.useConfigRanks == true) {
+    		Main.configRankString = config.getString("Application-Settings.Application-Ranks");
+    		for (String rankName : Main.configRankString.split(",")) {
+        		if ((rankName.contains("[")) && (rankName.contains("]"))) {
+        			List<String> splitName = new ArrayList<String>(Arrays.asList(rankName.substring(rankName.indexOf("["), rankName.indexOf("]")).split(".")));
+        			for (String nameVal : splitName) {
+        				if (nameVal.equals(rankName.substring(0, rankName.indexOf("]")).toUpperCase())) {
+        					splitName.remove(nameVal);
+        				}
+        			}
+        			splitName.add(rankName.substring(0, rankName.indexOf("]")).toUpperCase());
+        			Main.ranksEnum.put(rankName.substring(0, rankName.indexOf("]")), splitName);
+        		} else {
+        			Main.ranksEnum.put(rankName, Arrays.asList(rankName.toUpperCase()));
+        		}
+        		
+        	}
+    	}
 	}
 
 }
