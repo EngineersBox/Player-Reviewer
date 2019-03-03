87c41a531efc7bbd74aca92e967588672841f057
Jack Kilrain <toastcraft.info@gmail.com>
2019-03-03 13:28:17 +0100

    Added PlotInheritanceException.java

---
+++ src/me.engineersbox.playerrev.exceptions/PlotInheritanceException.java
@@ -0,0 +1,11 @@
+package me.engineersbox.playerrev.exceptions;
+
+public class PlotInheritanceException extends Exception {
+
+	private static final long serialVersionUID = 2343994793089142662L;
+
+	public PlotInheritanceException(String field) {
+		super(field);
+	}
+	
+}
