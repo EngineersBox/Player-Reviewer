72d99d7ed9a40680231e1f16accefc15800fb086
Jack Kilrain <toastcraft.info@gmail.com>
2019-03-10 13:36:59 +0100

    Changed in-file references of RankEnum to RankEnum.java (separate file)

--- src/me.engineersbox.playerrev/Commands.java
+++ src/me.engineersbox.playerrev/Commands.java
@@ -3,9 +3,7 @@ package me.engineersbox.playerrev;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -23,48 +21,6 @@ import me.engineersbox.playerrev.methodlib.Lib;
 import me.engineersbox.playerrev.mysql.SQLLink;
 
 public class Commands implements CommandExecutor {
-	
-	public enum RankEnum {
-		GUEST("GUEST"),
-		SQUIRE("SQUIRE"),
-		KNIGHT("KNIGHT"),
-		BARON("BARON"),
-		BUILDER("BUILDER"),
-		HEAD_BUILDER("HEAD_BUILDER"),
-		SENIOR_BUILDER("SENIOR_BUILDER");
-		
-	    private String[] aliases;
-
-	    private static Map<String, RankEnum> rankenum = new HashMap<>();
-	    static {
-	        for (RankEnum re : RankEnum.values()) {
-	            for (String alias : re.aliases) {
-	            	rankenum.put(alias, re);
-	            }
-	        }
-	    }
-
-	    private RankEnum(String ... aliases) {
-	        this.aliases = aliases;
-	    }
-
-	    public static RankEnum valueOfByAlias(String alias) {
-	    	RankEnum re = rankenum.get(alias);
-	        if (re == null) {
-	            throw new IllegalArgumentException("No enum alias " + RankEnum.class.getCanonicalName() + "." + alias);
-	        }
-	        return re;
-	    }
-	    
-	    public static boolean isValid(String alias) {
-	    	RankEnum re = rankenum.get(alias);
-	    	if (re == null) {
-	    		return false;
-	    	} else {
-	    		return true;
-	    	}
-	    }
-	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
