371ee6215db59261b147e142f83433b6ded56e7c
Jack Kilrain <toastcraft.info@gmail.com>
2019-03-12 15:04:56 +0100

    Added handling of variable criteria count via generic foreach loops in newApp() ratePlayer() and getRatings()

--- src/me.engineersbox.playerrev/InvConfig.java
+++ src/me.engineersbox.playerrev/InvConfig.java
@@ -13,6 +13,7 @@ import me.engineersbox.playerrev.Main;
 import me.engineersbox.playerrev.exceptions.FieldValueException;
 import me.engineersbox.playerrev.exceptions.PlotInheritanceException;
 import me.engineersbox.playerrev.methodlib.Lib;
+import me.engineersbox.playerrev.mysql.SQLConfig;
 
 public class InvConfig extends AbstractFile {
 
@@ -23,6 +24,7 @@ public class InvConfig extends AbstractFile {
     }
     
     public static void newApp(Player p, String pname, String rank) throws FieldValueException, PlotInheritanceException {
+    	
     	if (!config.contains(pname)) {
 	    	List<String> raters = new ArrayList<String>();
 	    	String coordsstring = null;
@@ -33,15 +35,14 @@ public class InvConfig extends AbstractFile {
 				coordsstring = Lib.getCoordsString(p.getLocation());
 			}
 	    	
+			List<String> criteriaList = SQLConfig.getCriteria();
 	    	config.set(pname + ".Rank", rank);
-	    	config.set(pname + ".Atmosphere", "0-0");
-	    	config.set(pname + ".Originality", "0-0");
-	    	config.set(pname + ".Terrain", "0-0");
-	    	config.set(pname + ".Structure", "0-0");
-	    	config.set(pname + ".Layout", "0-0");
+	    	for (String current : criteriaList) {
+	    		config.set(pname + "." + current, "0-0");
+	    	}
 	    	config.set(pname + ".PlotLoc", coordsstring);
 	    	config.set(pname + ".TotalRatings", "0");
-	    	raters.add("Name-0-0-0-0-0");
+	    	raters.add("Name-0");
 	    	config.set(pname + ".Raters", raters);
 	        saveConfig();
     	} else {
@@ -61,17 +62,17 @@ public class InvConfig extends AbstractFile {
     	
     }
     
-    public static void ratePlayer(String prater, String pname, Integer atmosphere, Integer originality, Integer terrain, Integer structure, Integer layout) throws FieldValueException {
+    public static void ratePlayer(String prater, String pname, List<Integer> criteria) throws FieldValueException {
     	
-    	if (config.getList(pname) != null) {
+    	if (config.get(pname) != null) {
     		
         	List<String> raters = config.getStringList(pname + ".Raters");
+        	List<String> configCriteria = SQLConfig.getCriteria();
+        	List<Integer> currentCriteria = new ArrayList<Integer>();
+        	for (String current : configCriteria) {
+        		currentCriteria.add(Integer.parseInt(config.getString(pname + "." + current).substring(config.getString(pname + "." + current).lastIndexOf("-") + 1)) + 1);
+        	}
         	
-        	int AtCount = Integer.parseInt(config.getString(pname + ".Atmosphere").substring(config.getString(pname + ".Atmosphere").lastIndexOf("-") + 1)) + 1;
-        	int OrCount = Integer.parseInt(config.getString(pname + ".Originality").substring(config.getString(pname + ".Originality").lastIndexOf("-") + 1)) + 1;
-        	int TrCount = Integer.parseInt(config.getString(pname + ".Terrain").substring(config.getString(pname + ".Terrain").lastIndexOf("-") + 1)) + 1;
-        	int StCount = Integer.parseInt(config.getString(pname + ".Structure").substring(config.getString(pname + ".Structure").lastIndexOf("-") + 1)) + 1;
-        	int LaCount = Integer.parseInt(config.getString(pname + ".Layout").substring(config.getString(pname + ".Layout").lastIndexOf("-") + 1)) + 1;
         	int cTotalRatings = Integer.parseInt(config.getString(pname + ".TotalRatings")) + 1;
         	
         	List<Integer> valuelist = new ArrayList<Integer>();
@@ -80,18 +81,21 @@ public class InvConfig extends AbstractFile {
 			} catch (SQLException e) {
 				throw new FieldValueException(pname);
 			}
-        	
-        	config.set(pname + ".Atmosphere", Float.toString((valuelist.get(0) + atmosphere) / cTotalRatings) + "-" + AtCount);
-        	config.set(pname + ".Originality", Float.toString((valuelist.get(1) + originality) / cTotalRatings) + "-" + OrCount);
-        	config.set(pname + ".Terrain", Float.toString((valuelist.get(2) + terrain) / cTotalRatings) + "-" + TrCount);
-        	config.set(pname + ".Structure", Float.toString((valuelist.get(3) + structure) / cTotalRatings) + "-" + StCount);
-        	config.set(pname + ".Layout", Float.toString((valuelist.get(4) + layout) / cTotalRatings) + "-" + LaCount);
+			
+			for (int i = 0; i < currentCriteria.size(); i++) {
+				config.set(pname + "." + configCriteria.get(i), Float.toString((valuelist.get(i) + criteria.get(i)) / cTotalRatings) + "-" + currentCriteria.get(i));
+			}
         	config.set(pname + ".TotalRatings", Integer.toString(cTotalRatings));
         	
-        	if (raters.get(0).equalsIgnoreCase("Name-0-0-0-0-0")) {
-        		raters.set(0, prater + "-" + atmosphere + "-" + originality + "-" + terrain + "-" + structure + "-" + layout);
+        	String val = "";
+    		for (Integer current : criteria) {
+    			val += "-" + current;
+    		}
+        	
+        	if (raters.get(0).equalsIgnoreCase("Name-0")) {
+        		raters.set(0, prater + val);
         	} else {
-        		raters.add(prater + "-" + atmosphere + "-" + originality + "-" + terrain + "-" + structure + "-" + layout);
+        		raters.add(prater + val);
         	}
         	
         	config.set(pname + ".Raters", raters);
@@ -107,25 +111,16 @@ public class InvConfig extends AbstractFile {
     
     public static ArrayList<List<String>> getRatings(String pname) throws FieldValueException {
     	
-    	if (config.getList(pname) != null) {
+    	if (config.get(pname) != null) {
     		
-        	List<String> raters = config.getStringList(pname + "raters");
-        	
-        	String cAtmosphere = config.getString(pname + ".Atmosphere").substring(0, config.getString(pname + ".Atmosphere").lastIndexOf("-"));
-        	String cOriginality = config.getString(pname + ".Originality").substring(0, config.getString(pname + ".Originality").lastIndexOf("-"));
-        	String cTerrain = config.getString(pname + ".Terrain").substring(0, config.getString(pname + ".Terrain").lastIndexOf("-"));
-        	String cStructure = config.getString(pname + ".Structure").substring(0, config.getString(pname + ".Structure").lastIndexOf("-"));
-        	String cLayout = config.getString(pname + ".Layout").substring(0, config.getString(pname + ".Layout").lastIndexOf("-"));
-        	String rank = config.getString(pname + ".Rank");
-        	
+        	List<String> raters = config.getStringList(pname + ".Raters");
+        	List<String> configCriteria = SQLConfig.getCriteria();
         	List<String> averages = new ArrayList<String>();
         	
-        	averages.add(cAtmosphere + " ");
-        	averages.add(cOriginality + " ");
-        	averages.add(cTerrain + " ");
-        	averages.add(cStructure + " ");
-        	averages.add(cLayout + " ");
-        	averages.add(rank);
+        	for (String current : configCriteria) {
+        		averages.add(config.getString(pname + "." + current).substring(0, config.getString(pname + "." + current).lastIndexOf("-")));
+        	}
+        	averages.add(config.getString(pname + ".Rank"));
         	
         	ArrayList<List<String>> retval = new ArrayList<List<String>>();
         	retval.add(raters);
