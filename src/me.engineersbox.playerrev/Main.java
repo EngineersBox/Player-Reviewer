17ec8b5a889939e5833a410dee3944283353c1e1
Jack Kilrain <toastcraft.info@gmail.com>
2019-07-23 13:09:05 +0200

    Added Map<T, T> objects for positions, cameras, parameters and chunklist and added command executors for pos1, pos2, get, cam, setparam, viewparams, clearparams

--- src/me.engineersbox.playerrev/Main.java
+++ src/me.engineersbox.playerrev/Main.java
@@ -3,14 +3,13 @@ package me.engineersbox.playerrev;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
@@ -23,6 +22,9 @@ import com.github.intellectualsites.plotsquared.api.PlotAPI;
 import org.bukkit.entity.Player;
 
 import me.engineersbox.playerrev.InvConfig;
+import me.engineersbox.playerrev.chunky.CameraObject;
+import me.engineersbox.playerrev.chunky.CoordsObject;
+import me.engineersbox.playerrev.chunky.JSONParameter;
 import me.engineersbox.playerrev.methodlib.DynamicEnum;
 import me.engineersbox.playerrev.methodlib.MaxSizeHashMap;
 import me.engineersbox.playerrev.mysql.MySQL;
@@ -30,220 +32,6 @@ import me.engineersbox.playerrev.mysql.Config;
 import me.engineersbox.playerrev.updater.SpigotUpdater;
 import me.engineersbox.playerrev.updater.Updaters;
 import me.lucko.luckperms.api.LuckPermsApi;
-
-class position {
-	
-    public double x = 0;
-    public double y = 0;
-    public double z = 0;
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public double getZ() {
-        return z;
-    }
-
-    public void setZ(double z) {
-        this.z = z;
-    }
-}
-
-class orientation {
-	
-    public double roll = 0;
-    public double pitch = 0;
-    public double yaw = 0;
-
-    public double getRoll() {
-        return roll;
-    }
-
-    public void setRoll(double roll) {
-        this.roll = roll;
-    }
-
-    public double getPitch() {
-        return pitch;
-    }
-
-    public void setPitch(double pitch) {
-        this.pitch = pitch;
-    }
-
-    public double getYaw() {
-        return yaw;
-    }
-
-    public void setYaw(double yaw) {
-        this.yaw = yaw;
-    }
-}
-class CameraObject {
-	
-    public String name = "camera 1";
-    public position position;
-    public orientation orientation;
-    public String projectionMode = "PINHOLE";
-    public float fov = 90.0f;
-    public String dof = "Infinity";
-    public float focalOffset = 2.0f;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public me.engineersbox.playerrev.position getPosition() {
-        return position;
-    }
-
-    public void setPosition(me.engineersbox.playerrev.position position) {
-        this.position = position;
-    }
-
-    public me.engineersbox.playerrev.orientation getOrientation() {
-        return orientation;
-    }
-
-    public void setOrientation(me.engineersbox.playerrev.orientation orientation) {
-        this.orientation = orientation;
-    }
-
-    public String getProjectionMode() {
-        return projectionMode;
-    }
-
-    public void setProjectionMode(String projectionMode) {
-        this.projectionMode = projectionMode;
-    }
-
-    public float getFov() {
-        return fov;
-    }
-
-    public void setFov(float fov) {
-        this.fov = fov;
-    }
-
-    public String getDof() {
-        return dof;
-    }
-
-    public void setDof(String dof) {
-        this.dof = dof;
-    }
-
-    public float getFocalOffset() {
-        return focalOffset;
-    }
-
-    public void setFocalOffset(float focalOffset) {
-        this.focalOffset = focalOffset;
-    }
-}
-class CoordsObject {
-	
-    public Location Position1 = null;
-    public Location Position2 = null;
-
-    public Location getPosition1() {
-        return Position1;
-    }
-
-    public void setPosition1(Location position1) {
-        Position1 = position1;
-    }
-
-    public Location getPosition2() {
-        return Position2;
-    }
-
-    public void setPosition2(Location position2) {
-        Position2 = position2;
-    }
-}
-
-class JSONParameter {
-	
-	private List<String> params = new ArrayList<String>();
-	private List<String> values = new ArrayList<String>();
-	
-	public JSONParameter(List<String> params, List<String> values) {
-		this.params = params;
-		this.values = values;
-	}
-	
-	public JSONParameter() {}
-	
-	public List<String> getParams() {
-		return this.params;
-	}
-	
-	public void setParams(List<String> newParams) {
-		this.params = newParams;
-	}
-	
-	public void addParam(String param) {
-		this.params.add(param);
-	}
-	
-	public List<String> getValues() {
-		return this.values;
-	}
-	
-	public void setValues(List<String> newValues) {
-		this.values = newValues;
-	}
-	
-	public void addValue(String value) {
-		this.params.add(value);
-	}
-	
-	public boolean replaceValue(String param, String value) {
-		if (this.params.contains(param)) {
-			int paramIndex = this.params.indexOf(param);
-			this.values.set(paramIndex, value);
-			return true;
-		}
-		return false;
-	}
-	
-	public void addParamValue(String param, String value) {
-		this.params.add(param);
-		this.values.add(value);
-	}
-	
-	public String toString() {
-		String retVal = "";
-		
-		if (params.size() > 0 && values.size() > 0) {
-			retVal += this.params.get(0) + ": " + this.values.get(0);
-			for (int i = 1; i < this.params.size(); i++) {
-				retVal += ", " + this.params.get(i) + ": " + this.values.get(i);
-			}
-		}
-		
-		return retVal;
-	}
-	
-}
 
 public class Main extends JavaPlugin implements Listener {
 	
@@ -270,9 +58,9 @@ public class Main extends JavaPlugin implements Listener {
 	
 	double magicChunkyNumber = 57.29577951308232;
 	public static Map<String, CoordsObject> positions = new HashMap<String, CoordsObject>();
-    public static MaxSizeHashMap<String, CameraObject> cameras;
-    public static int camCount = 0;
-    public static Map<String, JSONParameter> paramMap = new HashMap<String, JSONParameter>();
+	public static Map<UUID, MaxSizeHashMap<String, CameraObject>> cameras = new HashMap<UUID, MaxSizeHashMap<String, CameraObject>>();
+    public static Map<UUID, JSONParameter> paramMap = new HashMap<UUID, JSONParameter>();
+    public static Map<UUID, String> chunkList = new HashMap<UUID, String>();
 	
     public void onEnable() {
     	
@@ -328,7 +116,6 @@ public class Main extends JavaPlugin implements Listener {
     		}
     	}
     	
-    	cameras = new MaxSizeHashMap<String, CameraObject>(Config.maxCamCount());
     	Config.InitRankConfig();
     	
         getCommand("pr").setExecutor(new Commands());
@@ -345,10 +132,9 @@ public class Main extends JavaPlugin implements Listener {
         getCommand("pr pos2").setExecutor(new Commands());
         getCommand("pr cam").setExecutor(new Commands());
         getCommand("pr get").setExecutor(new Commands());
-        getCommand("rs").setExecutor(new Commands());
-        getCommand("rs setparam").setExecutor(new Commands());
-        getCommand("rs viewparams").setExecutor(new Commands());
-        getCommand("rs clearparams").setExecutor(new Commands());
+        getCommand("pr setparam").setExecutor(new Commands());
+        getCommand("pr viewparams").setExecutor(new Commands());
+        getCommand("pr clearparams").setExecutor(new Commands());
     }
     
     @Override
