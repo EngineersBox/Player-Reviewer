05ca2972dbe25ed1d0357c80985ecac51939ded8
Jack Kilrain <toastcraft.info@gmail.com>
2019-08-31 03:53:21 +0200

    Added getInProgressApps() and setInProgressApps()

--- src/me.engineersbox.playerrev.gitlab/GitConfig.java
+++ src/me.engineersbox.playerrev.gitlab/GitConfig.java
@@ -1,5 +1,11 @@
 package me.engineersbox.playerrev.gitlab;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 import me.engineersbox.playerrev.AbstractFile;
 import me.engineersbox.playerrev.Main;
 
@@ -31,6 +37,22 @@ public class GitConfig extends AbstractFile {
 	
 	public static String getRenderQuery() {
 		return config.getString("GitLab-Config.Logins.Render-Link-Queries");
+	}
+	
+	public static Map<UUID, String[]> getInProgressApps() {
+		List<String> ipApps = config.getStringList("GitLab-Config.In-Progress-Apps");
+		Map<UUID, String[]> apps = new HashMap<UUID, String[]>();
+		ipApps.forEach(val -> {
+			String[] vals = val.split(",");
+			apps.put(UUID.fromString(vals[0]), new String[] {vals[1], vals[2]});
+		});
+		return apps;
+	}
+	
+	public static void setInProgressApps(Map<UUID, String[]> apps) {
+		List<String> ipApps = new ArrayList<>();
+		apps.forEach((UUID id, String[] date) -> ipApps.add(id.toString() + "," + date[0] + "," + date[1]));
+		config.set("GitLab-Config.In-Progress-Apps", ipApps);
 	}
 
 }
