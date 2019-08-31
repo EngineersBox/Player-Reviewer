cce3eeace1ffdea11061e55cbe32fa167bb3f7cd
Jack Kilrain <toastcraft.info@gmail.com>
2019-08-31 03:54:45 +0200

    Fixed substring regex start/end values, cahnged return of getUpdateTime() to either empty string or substring depending on 'status' state

--- src/me.engineersbox.playerrev.gitlab/GitLabManager.java
+++ src/me.engineersbox.playerrev.gitlab/GitLabManager.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -14,9 +14,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import me.engineersbox.playerrev.Main;
 import me.engineersbox.playerrev.enums.GitLabStatusCodes;
-import me.engineersbox.playerrev.mysql.Config;
 
 public class GitLabManager {
 	
@@ -45,7 +43,7 @@ public class GitLabManager {
 		return !response.toString().equals("[]");
 	}
 	
-	private static String getIssueID(String title) throws IOException {
+	public static String getIssueID(String title) throws IOException {
 		String geturl = GitConfig.getGitAddress()
 				+ "/api/v4/projects/"
 				+ GitConfig.getProjectID() + "/"
@@ -108,10 +106,9 @@ public class GitLabManager {
 		}
 	}
 	
-	public static String getUpdateTime(Player p) {
+	public static String getUpdateTime(java.util.UUID id) {
 		try {
-			String geturl = GitConfig.getRenderLink().replaceAll("\\<UUID\\>", p.getUniqueId().toString())
-					+ GitConfig.getRenderQuery();
+			String geturl = GitConfig.getRenderLink().replaceAll("\\<UUID\\>", id.toString()) + GitConfig.getRenderQuery();
 			URL obj = new URL(geturl);
 			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 			
@@ -122,18 +119,23 @@ public class GitLabManager {
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			String inputLine;
 			String updated_on = "";
+			boolean isDone = false;
 			
 			while ((inputLine = in.readLine()) != null) {
+				if (inputLine.contains("\"status\": \"Done\"")) {
+					isDone = true;
+				}
 				if (inputLine.contains("sys_updated_on")) {
 					updated_on = StringUtils.substringBetween(inputLine, "\"sys_updated_on\": \"", "\",");
-					return updated_on;
 				}
 			}
 			
 			in.close();
 			con.disconnect();
-			
-			return updated_on;
+			if (isDone) {
+				return updated_on;
+			}
+			return "";
 		} catch (IOException e) {
 			Bukkit.getLogger().info("[Player Reviewer] GitLab issue creator: could not access \"sys_updated_on\" field");
 			return "";
@@ -141,18 +143,17 @@ public class GitLabManager {
 	}
 	
 	public static boolean compareDateTime(String date_one, String date_two) {
-		int hour1 = Integer.parseInt(date_one.charAt(11)+""+date_one.charAt(12));
-		int min1 = Integer.parseInt(date_one.charAt(14)+""+date_one.charAt(15));
-		int sec1 = Integer.parseInt(date_one.charAt(17)+""+date_one.charAt(18));
-		int hour2 = Integer.parseInt(date_two.charAt(11)+""+date_two.charAt(12));
-		int min2 = Integer.parseInt(date_two.charAt(14)+""+date_two.charAt(15));
-		int sec2 = Integer.parseInt(date_two.charAt(17)+""+date_two.charAt(18));
-		return hour2 >= hour1 && min2 >= min1 && sec2 >= sec1;
+		if (date_two.equals("")) {
+			return false;
+		}
+		LocalDateTime d_one = LocalDateTime.parse(date_one.replace(" ", "T") + ".000");
+		LocalDateTime d_two = LocalDateTime.parse(date_two.replace(" ", "T") + ".000");
+		return d_two.isAfter(d_one);
 	}
 	
-	public static List<String> getRenderLinks(Player p) throws IOException, NoSuchFieldException {
-		String geturl = GitConfig.getRenderLink().replaceAll("\\<UUID\\>", p.getUniqueId().toString())
-				+ "?" + GitConfig.getRenderQuery();
+	public static List<String> getRenderLinks(java.util.UUID id) throws IOException {
+		String geturl = GitConfig.getRenderLink().replaceAll("\\<UUID\\>", id.toString().toString())
+				+ "?" + GitConfig.getRenderQuery().replace("?", "");
 		URL obj = new URL(geturl);
 		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 		
@@ -163,45 +164,26 @@ public class GitLabManager {
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String inputLine;
 		List<String> camList = new ArrayList<>();
-		String created_on = "";
-		String updated_on = "";
 		boolean isDone = false;
-		String desc = getIssueDescription("Application%20for%20" + p.getName());
-		String datetime = StringUtils.substringBetween(desc, "\\*\\*Date Time\\*\\*: ", "\\\\u003cbr\\/\\\\u003e");
+		int cCount = 1;
 		
 		while ((inputLine = in.readLine()) != null) {
 			if (inputLine.contains("\"status\": \"Done\"")) {
 				isDone = true;
 			}
 			if (isDone) {
-				if (inputLine.contains("sys_updated_on")) {
-					updated_on = StringUtils.substringBetween(inputLine, "\"sys_updated_on\": \"", "\",");
-					if (!compareDateTime(datetime, updated_on)) {
-						throw new NoSuchFieldException();
+				if (inputLine.contains("\"cam_" + cCount + "\"")) {
+					String sub = StringUtils.substringBetween(inputLine, "\"cam_" + cCount + "\": \"", "\",");
+					if (sub != null) { 
+						camList.add(sub);
 					}
-				} else if (inputLine.contains("sys_created_on")) {
-					created_on = StringUtils.substringBetween(inputLine, "\"sys_created_on\": \"", "\",");
-				}
-				if (!created_on.equals(updated_on) && !created_on.equals("") && !updated_on.equals("")) {
-					int[] camCount = IntStream.range(1, Config.maxCamCount()).toArray();
-					for (int c : camCount) {
-						if (inputLine.contains("\"cam_" + c + "\"")) {
-							String sub = StringUtils.substringBetween(inputLine, "\"cam_" + c + "\": \"", "\",");
-							if (sub != null) { 
-								camList.add(sub);
-							}
-						}
-					}
+					cCount++;
 				}
 			}
 		}
 		
 		in.close();
 		con.disconnect();
-		
-		if (camList.size() > 0) {
-			Main.renderChecks.put(p.getUniqueId(), new String[]{updated_on, created_on});
-		}
 		
 		return camList;
 	}
@@ -294,6 +276,7 @@ public class GitLabManager {
 					+ "issues/" + getIssueID(title.replaceAll("\\s", "%20"));
 			URL obj = new URL(delurl);
 			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+			
 			con.setRequestMethod("DELETE");
 			con.setRequestProperty("PRIVATE-TOKEN", GitConfig.getAccesskey());
 			con.setDoOutput(true);
@@ -316,19 +299,19 @@ public class GitLabManager {
 	
 	public static void editIssue(Player p, String title, String desc) {
 		try {
-			String posturl = GitConfig.getGitAddress()
+			String puturl = GitConfig.getGitAddress()
 					+ "/api/v4/projects/"
 					+ GitConfig.getProjectID() + "/"
 					+ "issues/" + getIssueID(title.replaceAll("\\s", "%20"))
 					+ "?description=" + desc;
-			URL obj = new URL(posturl);
+			URL obj = new URL(puturl);
 			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-
+			
 			con.setRequestMethod("PUT");
 			con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
 			con.setRequestProperty("PRIVATE-TOKEN", GitConfig.getAccesskey());
 			con.setDoOutput(true);
-
+			
 			int responseCode = con.getResponseCode();
 			con.disconnect();
 			
